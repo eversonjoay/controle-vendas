3e5f56cf709c3b55cf55ca14e6d4b22104efd541
Everson Joay <eversonjoay@gmail.com>
2023-07-16 22:25:25 -0300

    Added ngFor on select, pipe data formatting

--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -1,3 +1,7 @@
 export class Constants {
   public static readonly PEDIDOS_KEY = 'pedidos';
+
+  public static readonly NAO_PAGO = 'Não pago';
+  public static readonly PAGO_PARCIAL = 'Parcialmente pago';
+  public static readonly PAGO = 'Pago';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { enableProdMode } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR'}]
+})
   .catch(err => console.error(err));
